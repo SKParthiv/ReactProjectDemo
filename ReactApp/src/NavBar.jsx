8bb2762a0a050bfcad81a9133d55fe93b234a52c
SKParthiv <142598464+SKParthiv@users.noreply.github.com>
2023-12-05 23:17:18 +0530

    Update

---
+++ ReactApp/src/NavBar.jsx
@@ -0,0 +1,12 @@
+import Link from 'react/link'
+
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link></Link></li>
+            </ul>
+        </nav>
+    )
+}
